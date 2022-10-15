d80abc4cec4f7ebb0e762a3a6e833100733b8bd7
nurmukhammaddev <nurmuhammadpy@gmail.com>
2022-10-15 14:42:24 +0500

    bot

---
+++ handlers/users/start.py
@@ -0,0 +1,14 @@
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+from loader import dp
+
+
+@dp.message_handler(CommandStart())
+async def bot_start(message: types.Message):
+    await message.answer(f"Salom, {message.from_user.full_name}!\n"
+                         "\n"
+                         f"Bot Instagram va Tiktokdan postlarni yuklab oladi\n"
+                         "\n"
+                         f"Havolani yuboring!")
+
+
---
+++ insta.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+
+def instadownloader(link):
+    url = "https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index"
+
+    querystring = {"url": link}
+
+    headers = {
+        "X-RapidAPI-Key": "54e262782emsh7c9b12dd7f332b4p158bd6jsnaee732e6c4a8",
+        "X-RapidAPI-Host": "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    rest = json.loads(response.text)
+    if 'error' in rest:
+        return "Error"
+    else:
+        dict = {}
+        if rest['Type'] == 'Post-image':
+            dict['type'] = 'image'
+            dict['media'] = rest['media']
+            return dict
+
+        elif rest['Type'] == 'Post-Video':
+            dict['type'] = 'video'
+            dict['media'] = rest['media']
+            return dict
+        
+        elif rest['Type'] == 'Carousel':
+            dict['type'] = 'carousel'
+            dict['media'] = rest['media']
+            return dict
+        else:
+            return "Error"
+
