87ddca09eda5c22a31c0742e5fe375c11a21b652
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2022-10-22 14:13:57 +0500

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,16 +1,20 @@
 import requests
 import json
+
 def instadownloader(link):
     url = "https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index"
-    querystring = {"url":"link"}
-	headers = {	
-	        "X-RapidAPI-Key": "d179273d7emsh34fe0f35151a7a3p12379ejsnf9a526d26356",
-	        "X-RapidAPI-Host": "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com"
-	}
+
+    querystring = {"url": link}
+
+    headers = {
+        "X-RapidAPI-Key": "d179273d7emsh34fe0f35151a7a3p12379ejsnf9a526d26356",
+        "X-RapidAPI-Host": "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com"
+    }
+
     response = requests.request("GET", url, headers=headers, params=querystring)
     rest = json.loads(response.text)
     if 'error' in rest:
-        return "Error"
+        return "error"
     else:
         dict = {}
         if rest['Type'] == 'Post-image':
@@ -22,7 +26,7 @@ def instadownloader(link):
             dict['type'] = 'video'
             dict['media'] = rest['media']
             return dict
-        
+
         elif rest['Type'] == 'Carousel':
             dict['type'] = 'carousel'
             dict['media'] = rest['media']
