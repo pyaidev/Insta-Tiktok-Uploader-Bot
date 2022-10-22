bd630db7c377dc8833ccae7e8ebc6f9458655e47
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2022-10-22 15:21:33 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,12 +1,14 @@
-from aiogram import Bot, Dispatcher, executor, types, executor
+from aiogram import types, Bot
 from aiogram.dispatcher.filters.builtin import CommandStart
-from loader import dp
 
+from data.config import BOT_TOKEN
+from loader import dp
+bot = Bot(BOT_TOKEN)
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
+    user_id = message.from_user.id
     await bot.send_sticker(user_id, sticker='CAACAgIAAxkBAAEEeItiWG7TWqCapeRnGLmb0JhzlfO6UwACAQEAAladvQoivp8OuMLmNCME')
     await message.answer(f"Salom, {message.from_user.full_name}!\n"
                          f"Havolani yuboring!")
-
 
