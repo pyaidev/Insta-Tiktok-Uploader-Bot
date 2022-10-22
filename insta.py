2899d147223d04d1e665c33faf41add2f9d990f0
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2022-10-22 12:56:43 +0500

    Update insta.py

--- insta.py
+++ insta.py
@@ -3,9 +3,9 @@ import json
 def instadownloader(link):
     url = "https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index"
     querystring = {"url":"link"}
-	headers = {
-	"X-RapidAPI-Key": "d179273d7emsh34fe0f35151a7a3p12379ejsnf9a526d26356",
-	"X-RapidAPI-Host": "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com"
+	headers = {	
+	        "X-RapidAPI-Key": "d179273d7emsh34fe0f35151a7a3p12379ejsnf9a526d26356",
+	        "X-RapidAPI-Host": "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com"
 	}
     response = requests.request("GET", url, headers=headers, params=querystring)
     rest = json.loads(response.text)
