5769259ac00aed75a16e272bee07ea248e90d39e
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2022-10-22 15:11:40 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -5,10 +5,8 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
+    await bot.send_sticker(user_id, sticker='CAACAgIAAxkBAAEEeItiWG7TWqCapeRnGLmb0JhzlfO6UwACAQEAAladvQoivp8OuMLmNCME')
     await message.answer(f"Salom, {message.from_user.full_name}!\n"
-                         "\n"
-                         f"Bot Instagram va Tiktokdan postlarni yuklab oladi\n"
-                         "\n"
                          f"Havolani yuboring!")
 
 
