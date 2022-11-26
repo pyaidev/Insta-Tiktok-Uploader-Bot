0ab16afa7bfb098c2cab662bc49bcf3a4b7c48f3
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2022-11-26 11:31:52 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -8,7 +8,7 @@ bot = Bot(BOT_TOKEN)
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     user_id = message.from_user.id
-    await bot.send_sticker(user_id, sticker='🔍')
+    await bot.send_sticker(user_id, sticker='CAACAgIAAxkBAAEEeItiWG7TWqCapeRnGLmb0JhzlfO6UwACAQEAAladvQoivp8OuMLmNCME')
     await message.answer(f"Hello, {message.from_user.full_name}!\n"
                          f"Send a link!")
 
