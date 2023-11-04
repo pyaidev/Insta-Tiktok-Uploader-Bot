230bb2b88c046b0b91aaff10fc7fd53b572a91b1
Nurmuhammad Mashrapov <nurmuhammad@MacBook-Pro---user.local>
2023-11-04 15:54:41 +0500

    multialanguage

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,14 +1,54 @@
 from aiogram import types, Bot
-from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.dispatcher.filters.builtin import CommandStart, Command
 
 from data.config import BOT_TOKEN
 from loader import dp
+
 bot = Bot(BOT_TOKEN)
+
+# Dictionary to map language codes to human-readable language names
+language_choices = {
+    'ru': 'Русский',
+    'uz': 'Oʻzbekcha',
+    'en': 'English',
+}
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    user_id = message.from_user.id
-    await bot.send_sticker(user_id, sticker='CAACAgIAAxkBAAEEeItiWG7TWqCapeRnGLmb0JhzlfO6UwACAQEAAladvQoivp8OuMLmNCME')
-    await message.answer(f"Hello, {message.from_user.full_name}!\n"
-                         f"Send a link!")
+    # Display language selection buttons
+    keyboard = types.InlineKeyboardMarkup()
+    for code, lang in language_choices.items():
+        keyboard.add(types.InlineKeyboardButton(text=lang, callback_data=f'set_lang_{code}'))
+    await message.answer("Please select your preferred language:", reply_markup=keyboard)
 
+@dp.callback_query_handler(lambda c: c.data.startswith('set_lang_'))
+async def set_language(call: types.CallbackQuery):
+    # Extract the selected language code
+    language_code = call.data.split('_')[-1]
+
+    # Set the user's language preference
+    user_id = call.from_user.id
+    user_language = language_code  # You can store this in a database for each user
+    # Replace this with your code to store the user's language preference
+
+    # Load the corresponding localization file
+    try:
+        with open(f'locales/{user_language}.json', 'r', encoding='utf-8') as file:
+            translation = json.load(file)
+    except FileNotFoundError:
+        # If the user's language is not supported, fall back to English
+        with open('locales/en.json', 'r', encoding='utf-8') as file:
+            translation = json.load(file)
+
+    # Get the translated message
+    start_message = translation.get('start_message', 'Hello, {full_name}!\nSend a link!')
+
+    # Replace placeholders with user-specific data
+    start_message = start_message.format(full_name=call.from_user.full_name)
+
+    # Send the message
+    await bot.send_message(user_id, start_message)
+
+if __name__ == "__main__":
+    from aiogram import executor
+    executor.start_polling(dp, skip_updates=True)
